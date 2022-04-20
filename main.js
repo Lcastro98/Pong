d44e2cbce4dfa2a6e20e66eb88e5fa23093f1a0e
Andrea Lorena Castro Manzano <lcastro0398@gmail.com>
2022-04-19 23:13:11 -0500

    Se agrega la barra espaciadora como iniciadora del juego

--- main.js
+++ main.js
@@ -6,6 +6,7 @@
         this.game_over = false;
         this.bars = [];
         this.ball = null;
+        this.playing = false;
     }
 
     self.Board.prototype = {
@@ -22,11 +23,19 @@
         this.x = x;
         this.y = y;
         this.radius = radius;
-        this.speed_x = 0;
-        this.speed_y = 3;
+        this.speed_x = 3;
+        this.speed_y = 0;
         this.board = board;
+        this.direction = 1;
         board.ball = this;
         this.kind = "circle";
+    }
+
+    self.Ball.prototype = {
+        move: function(){
+            this.x += (this.speed_x * this.direction);
+            this.y += (this.speed_y);
+        }
     }
 })();
 
@@ -74,8 +83,11 @@
             };
         },
         play: function(){
-            this.clean()
-            this.draw();
+            if(this.board.playing){
+                this.clean()
+                this.draw();
+                this.board.ball.move();
+            }
         }
     }
 
@@ -102,22 +114,33 @@ var board_view = new BoardView(canvas, board);
 var ball = new Ball(350, 200, 10, board);
 
 document.addEventListener("keydown", function(event){
-    event.preventDefault();
+
     if(event.key === "w"){
+        event.preventDefault();
         bar1.up();
     }
     else if(event.key === "s"){
+        event.preventDefault();
         bar1.down();
     }
-    else if(event.key === "ArrowUp"){
+    else if(event.code === "ArrowUp"){
+        event.preventDefault();
         bar2.up();
     }
-    else if(event.key === "ArrowDown"){
+    else if(event.code === "ArrowDown"){
+        event.preventDefault();
         bar2.down();
+    }
+    else if(event.code === "Space"){
+        event.preventDefault();
+        board.playing = !board.playing;
     }
 });
 
+board_view.draw();
+
 window.requestAnimationFrame(controller);
+
 
 function controller(){
     board_view.play();
